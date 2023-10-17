89ced8913b112a9e573513319bf10213a1776aed
Gauravrk215 <gauravrk215@gmail.com>
2023-10-17 05:54:41 +0000

    base_project

---
+++ gtest3_stest2_f/authsec_angular/frontend/angular-clarity-master/src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: true,
+  appName: 'My Application',
+  version: '11.2.13',
+  subVersion: '2021.05.13-01',
+  apiUrl: 'http://localhost:3000',
+  whiteUrl: 'http://localhost:3000',
+  blackUrl: 'http://localhost:3000/login',
+  // changeble url // port
+ 
+  //Deployment configs
+//   backport:'30166', // port
+//   backendUrl:'http://43.205.154.152:30166',
+
+  
+};
---
+++ gtest3_stest2_f/authsec_angular/frontend/angular-clarity-master/src/environments/environment.ts
@@ -0,0 +1,29 @@
+export const environment = {
+  production: false,
+  appName: 'My Application',
+  version: '11.2.13',
+  subVersion: '2021.05.13-01',
+  apiUrl: 'http://localhost:3000',
+  whiteUrl: 'http://localhost:3000',
+  blackUrl: 'http://localhost:3000/login',
+
+  // changeblelocal configs url
+ 
+  regiurl:'http://43.205.154.152:31165/',// registrationapp for frontend
+  registrationurl: 'http://localhost:3000',//used in jobqueue dropdown
+ 
+
+  //Deployment configs
+//   backport:'30166', // port
+//   backendUrl:'http://43.205.154.152:30166',
+//   jobUrl:'http://43.205.154.152:31170',
+// regiurl:'http://43.205.154.152:31165/',// registrationapp for frontend
+//  registrationurl: 'http://localhost:3000',//used in jobqueue dropdown
+//  sureops: 'http://43.205.154.152:31123',//used in jobqueue dropdown
+//   cretedevlopmenturl:'http://43.205.154.152:31170',
+// chaturl:'http://43.205.154.152:30173/#/main/board/0', //chat frontend
+//   jobmgurl:'http://43.205.154.152:31170',
+//  appbuilderurl:'http://43.205.154.152:31170',
+//    surejobUrl:'http://43.205.154.152:31170',
+//   communicationurl:'http://43.205.154.152:31170',
+};
